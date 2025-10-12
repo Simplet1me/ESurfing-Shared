23940ba4a5c372e594f6f3f2fef9d63b81777b47
Simplet1me <110585617+Simplet1me@users.noreply.github.com>
2025-10-12 13:51:30 +0800

    1
    1

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd D:\SchoolNet
+java -jar client.jar -u 3124004148 -p 11252013
+pause
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /media/simpletime/SN560/SchoolNet
+java -jar client.jar -u 用户名 -p 密码
